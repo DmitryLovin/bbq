bf3a578804aac40e4efd80ff5c3c4787d339be12
dmitrylovin <dmitrylovin@gmail.com>
2022-12-21 17:24:10 +0100

    user name to registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   helper_method :current_user_can_edit?
 
   def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[ email name password password_confirmation ])
+
     devise_parameter_sanitizer.permit(:account_update, keys: %i[ password password_confirmation current_password ])
   end
 
