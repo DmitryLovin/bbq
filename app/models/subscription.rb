5fb0ae6969460848463ef896fc4fd366dc328194
dmitrylovin <dmitrylovin@gmail.com>
2022-12-25 12:43:40 +0100

    validate methods to private, remove ;, locales fix

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -13,18 +13,6 @@ class Subscription < ApplicationRecord
   validates :user_email, uniqueness: { scope: :event_id }, unless: -> { user.present? }
   validate :foreign_email, unless: -> { user.present? }
 
-  def foreign_email
-    if User.find_by(email: user_email).present?
-      errors.add(:user_email, I18n.t("activerecord.validates.subscriptions.foreign_email"));
-    end
-  end
-
-  def self_subscription
-    if event.user == user
-      errors.add(:user, I18n.t("activerecord.validates.subscriptions.self_subscription"));
-    end
-  end
-
   def user_name
     if user.present?
       user.name
@@ -38,6 +26,20 @@ class Subscription < ApplicationRecord
       user.email
     else
       super
+    end
+  end
+
+  private
+
+  def foreign_email
+    if User.find_by(email: user_email).present?
+      errors.add(:base, :foreign_email)
+    end
+  end
+
+  def self_subscription
+    if event.user == user
+      errors.add(:base, :self_subscription)
     end
   end
 end
