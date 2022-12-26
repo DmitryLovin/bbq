2288d09430937d5ae30243ec71a197ae01b889ab
dmitrylovin <dmitrylovin@gmail.com>
2022-12-26 20:43:04 +0100

    secrets

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,14 @@
 if Rails.env.production?
+  secrets = YAML::load_file('config/secrets.yml')
   CarrierWave.configure do |config|
     config.fog_provider = "fog/aws"
 
     config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+      aws_access_key_id:     secrets['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: secrets['AWS_SECRET_ACCESS_KEY']
     }
 
-    config.fog_directory  = ENV['AWS_BUCKET_NAME']
+    config.fog_directory  = secrets['AWS_BUCKET_NAME']
   end
 end
