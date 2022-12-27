565602d7c68b7827ca486a6d9b1b9d9a1a0d52d5
dmitrylovin <dmitrylovin@gmail.com>
2022-12-27 18:12:04 +0100

    dont notify comment author

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -31,6 +31,7 @@ class CommentsController < ApplicationController
 
   def notify_subscribers(event, comment)
     all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
+    all_emails -= [comment.user.email] if comment.user.present?
 
     all_emails.each do |mail|
       EventMailer.comment(event, comment, mail).deliver_later
