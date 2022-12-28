8cb2099c320435147db32eb7514c86f1ba3155b8
dmitrylovin <dmitrylovin@gmail.com>
2022-12-28 17:52:37 +0100

    img gallery

---
+++ app/assets/javascripts/slb.js
@@ -0,0 +1,1 @@
+new SimpleLightbox(".gallery a", {overlayOpacity: 0.8, captionsData: "content"});
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,4 @@
 
 import "./controllers";
 import * as bootstrap from "bootstrap"
+require("simplelightbox")
