e564df3478822c9f4d5f19f20f5cf90bab5a10d8
dmitrylovin <dmitrylovin@gmail.com>
2023-01-18 13:09:46 +0100

    delete event fix

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,7 @@ class Event < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :subscriptions, dependent: :destroy
   has_many :subscribers, through: :subscriptions, source: :user
-  has_many :photos
+  has_many :photos, dependent: :destroy
 
   validates :user, presence: true
   validates :title, presence: true, length: { maximum: 255 }
