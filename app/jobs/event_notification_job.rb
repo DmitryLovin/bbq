2862dda05d5fbb23d23bb3f70c7ef4db764d736a
dmitrylovin <dmitrylovin@gmail.com>
2023-01-26 15:37:29 +0100

    EventNotifictaionJob refactor

--- app/jobs/event_notification_job.rb
+++ app/jobs/event_notification_job.rb
@@ -2,14 +2,12 @@ class EventNotificationJob < ApplicationJob
   queue_as :default
 
   def perform(object, mail = nil)
-    if mail.nil?
-      all_emails = (object.event.subscriptions.map(&:user_email) + [object.event.user.email]).uniq
-      all_emails -= [object.user&.email]
-      all_emails.each do |email|
-        EventMailer.try(object.class.to_s.downcase, object, email).deliver_now
-      end
-    else
-      EventMailer.try(object.class.to_s.downcase, object, mail).deliver_now
+    return EventMailer.public_send(object.class.model_name.singular, object, mail).deliver_now until mail.nil?
+
+    all_emails = (object.event.subscriptions.map(&:user_email) + [object.event.user.email]).uniq
+    all_emails -= [object.user&.email]
+    all_emails.each do |email|
+      EventMailer.public_send(object.class.model_name.singular, object, email).deliver_now
     end
   end
 end
